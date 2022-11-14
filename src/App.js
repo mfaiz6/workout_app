f91683e7869be365c326e4f194539e8d09dd067e
Mohammad Faiz Ahanger <Mohammadfaiz_2019bcse002@nitsri.net>
2022-11-14 13:54:48 +0530

    Add assets,  pages and components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import { Box } from '@mui/material'
+import ExerciseDetail from './pages/ExerciseDetail.jsx'
+import Home from './pages/Home.jsx'
+import Navbar from './components/Navbar.jsx'
+import Footer from './components/Footer.jsx'
+
 
 function App() {
   return (
-    <div>
-      App
-    </div>
+    <Box width="400px" sx={{ width: { xl : '1488px' }}} m="auto">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercises/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+
+    </Box>
   );
 }
 
